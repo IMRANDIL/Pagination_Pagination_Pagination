e40c0125d52e07fbd0cc51d7e10486d990acb353
IMRANDIL <aliimranadil2@gmail.com>
2022-01-21 18:38:27 +0530

    yh

--- pagination.js
+++ pagination.js
@@ -47,7 +47,7 @@ const loadDataIntoTable = (data) => {
             `<tr>
            <td style='font-size:20px;'>${colName[index]} (${colSymbol[index]})</td>
            <td style='font-size:20px;'>${rankCol[index]}</td>
-           <td style='font-size:25px;'>${priceCol[index]}</td>
+           <td style='font-size:25px;'>$${priceCol[index]}</td>
            <td class=${percentChange[index] > 0 ? 'green-text text-darken-4' : 'red-text text-darken-4'} style='font-size:25px;'>${percentChange[index]}</td>
            </tr>
            `
@@ -61,6 +61,67 @@ const loadDataIntoTable = (data) => {
 
 }
 
+//handle number click...
+
+function handleNumberClick(clickedLink, leftArrow, rightArrow) {
+
+    clickedLink.parentElement.classList = 'active';
+
+    let clickedLinkPageNumber = parseInt(clickedLink.innerText);
+
+    const url = getUrl((clickedLinkPageNumber * 10) - 10);
+    getData(url);
+
+    switch (clickedLinkPageNumber) {
+        case 1:
+            disableleftArrow();
+            if (rightArrow.className.indexOf('disabled') !== -1) {
+                enableleftArrow();
+            }
+            break;
+        case 10:
+            disablerightArrow();
+            if (leftArrow.className.indexOf('disabled') !== -1) {
+                enableleftArrow();
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+const handleleftArrowClick = () => {
+
+}
+
+const disableleftArrow = () => {
+
+}
+
+
+const enableleftArrow = () => {
+
+}
+
+
+
+const handlerightArrowClick = () => {
+
+}
+
+
+const disablerightArrow = () => {
+
+}
+
+
+const enablerightArrow = () => {
+
+}
+
+
 
 const init = () => {
     const url = getUrl();
@@ -71,7 +132,46 @@ init();
 
 
 
-//pagination section.....
+//pagination section.....handle the pagination..
+
+
+let pageLink = document.querySelectorAll('a');
+let activePageNumber;
+let clickedLink;
+let nextPage;
+let leftArrow;
+let rightArrow;
+let url = '';
+let activeLink;
+
+
+pageLink.forEach((link) => {
+    link.addEventListener('click', function () {
+        leftArrow = document.querySelector('.arrow-left');
+        rightArrow = document.querySelector('.arrow-right');
+        activeLink = document.querySelector('.active');
+
+        //get active page number....
+
+        activePageNumber = parseInt(activeLink.innerText);
+
+        if ((this.innerText === 'chevron_left' && activePageNumber === 1) || (this.innerText === 'chevron_right' && activePageNumber === 10)) return;
+
+        //update active class....
+
+        activeLink.classList = 'waves-effect';
+        activeLink.classList.remove('active');
+
+        if (this.innerText === 'chevron_left') {
+            handleleftArrowClick();
+        } else if (this.innerText === 'chevron_right') {
+            handlerightArrowClick();
+        } else {
+            handleNumberClick(this, leftArrow, rightArrow);
+        }
+
+    })
+})
 
 
 
